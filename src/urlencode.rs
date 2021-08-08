cb16d80a17cbb040878cc800a98f8f96246dcd7b
Galaxtone <galaxtone.plays.minecraft@gmail.com>
2021-08-08 18:27:45 -0400

    Updated Cargo.toml and added license

--- src/urlencode.rs
+++ src/urlencode.rs
@@ -1,3 +1,21 @@
+/*
+Pirate Realm, An experimental classicube server.
+Copyright (c) 2021  Galaxtone, Exopteron
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 fn to_hex(byte: u8) -> [char; 3] {
   let high = byte >> 4;
   let high =
